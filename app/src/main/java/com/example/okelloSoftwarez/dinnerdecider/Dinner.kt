fd60e7c528568fffd08959db50a1afef41164407
okelloEnos <enosokello@gmail.com>
2019-07-15 18:24:06 +0300

    initial commit

---
+++ app/src/main/java/com/example/okelloSoftwarez/dinnerdecider/Dinner.kt
@@ -0,0 +1,25 @@
+package com.example.okelloSoftwarez.dinnerdecider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class Dinner : AppCompatActivity() {
+    private val foodlist = arrayListOf("Chinesse","Hamburger","pololo","Pizza","Barros Pizza")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decinebtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodlist.count())
+            selectedFoodTest.text = foodlist[randomFood]
+        }
+        addfoodbtn.setOnClickListener {
+            val newFood = addfoodtxt.text.toString()
+            foodlist.add(newFood)
+            addfoodtxt.text.clear()
+        }
+    }
+}
